53f53b660ec6c6c76a7ad9e5b0f74d263230723e
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-15 17:26:35 +0900

    주석 업데이트

--- AttackMonster.cs
+++ AttackMonster.cs
@@ -40,20 +40,20 @@ public class AttackMonster : Monster
     {
         if (!TotalGameManager.isPlayerDead)
         {
-            currentChaseTime = 0;
-            TargetChase(theFieldOfViewAngle.ReturnPlayerPos());
+            currentChaseTime = 0; //초기화
+            TargetChase(theFieldOfViewAngle.ReturnPlayerPos()); //플레이어 위치를 받아 추격
 
-            while (currentChaseTime < chaseTime)
+            while (currentChaseTime < chaseTime) //추격시간이라면
             {
-                TargetChase(theFieldOfViewAngle.ReturnPlayerPos());
+                TargetChase(theFieldOfViewAngle.ReturnPlayerPos()); //플레이어 위치를 받아 추격
                 // 플레이어와 충분히 가까이 있나 
-                if (Vector3.Distance(transform.position, theFieldOfViewAngle.ReturnPlayerPos()) <= attackDistance)
+                if (Vector3.Distance(transform.position, theFieldOfViewAngle.ReturnPlayerPos()) <= attackDistance) //몬스터와 플레이어 사이의 거리가 attackdistace 사이에 있다면
                 {
                     if (theFieldOfViewAngle.Sight() && !isDead)  // 눈 앞에 있을 경우
-                        StartCoroutine(ATTACKCO());
+                        StartCoroutine(ATTACKCO()); //공격시작
                 }
-                yield return new WaitForSeconds(chaseDelayTime);
-                currentChaseTime += chaseDelayTime;
+                yield return new WaitForSeconds(chaseDelayTime); //대기
+                currentChaseTime += chaseDelayTime;//다시 추격시작
             }
             isChasing = false;
             isRunning = false;
@@ -65,24 +65,24 @@ public class AttackMonster : Monster
     protected IEnumerator ATTACKCO()
     {
         PlaySE(sound_Normal);
-        isAttacking = true;
-        nav.ResetPath();
+        isAttacking = true; //함수가 불러와지면 공격 활성화
+        nav.ResetPath(); //도중에 가던 목적지 초기화
         currentChaseTime = chaseTime;
-        yield return new WaitForSeconds(0.5f);
-        transform.LookAt(theFieldOfViewAngle.ReturnPlayerPos());
-        anim.SetTrigger("Attack");
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.5f); //잠깐 대기후
+        transform.LookAt(theFieldOfViewAngle.ReturnPlayerPos()); //플레이어를 바라보고
+        anim.SetTrigger("Attack"); //공격
+        yield return new WaitForSeconds(0.5f);// 잠깐 대기후 대기를 안하면 공격이 연속으로 되어 피가 엄청나게 깎인다.
         RaycastHit _hit;
-        if (Physics.Raycast(transform.position + Vector3.up, transform.forward, out _hit, attackDistance, targetMask))
+        if (Physics.Raycast(transform.position + Vector3.up, transform.forward, out _hit, attackDistance, targetMask)) //전방으로 빔을 쏴서 맞췄다면
         {
-            theStatus.DecreaseHP(attackDamage);
-            StartCoroutine(theStatus.ImageCO());
+            theStatus.DecreaseHP(attackDamage);//플레이어 체력 깎암
+            StartCoroutine(theStatus.ImageCO()); //ui활성화
            // hitanim.Play("hitanim", -1, 1);
         }
 
-        yield return new WaitForSeconds(attackDelay);
+        yield return new WaitForSeconds(attackDelay); //attackdelay만큼 대기
         isAttacking = false;
-            StartCoroutine(CHASETARGETCO());
+            StartCoroutine(CHASETARGETCO()); //다시 추격시작
     }
    
 }
