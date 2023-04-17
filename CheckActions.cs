2ad373fa2e6d2cedcb7cf36a380464b7eb73354b
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-17 14:56:02 +0900

    주석 업데이트

--- CheckActions.cs
+++ CheckActions.cs
@@ -19,54 +19,54 @@ public class CheckActions : MonoBehaviour
 
     private void TryAction()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) //e를 누를경우 
         {
-            CheckItem();
-            PickUp();
+            CheckItem(); //아이템을 매프레임 확인하고
+            PickUp(); //주울 수 있게함
         }
     }
 
     private void CheckItem()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask))
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask)) //레이어 마스크가 아이템인지 확인하고, 플레이어 위치에서 앞으로 빔을 쏨
         {
-            if (hitInfo.transform.tag == "Item")
+            if (hitInfo.transform.tag == "Item") //레이어마스크도 일치하고 태그도 아이템이면
             {
-                ShowtheInfo();
+                ShowtheInfo(); //아이템 정보를 보여줌
             }
         }
         else
-            HideInfo();
+            HideInfo(); //그외의 경우 아이템 정보를 숨김
     }
 
     private void ShowtheInfo()
     {
         pickupActivated = true;
         actionText.gameObject.SetActive(true);
-        actionText.text = "<color=yellow>"+ "(E)" +"</color>"+"  GET  " +hitInfo.transform.GetComponent<PickUP>().item.itemName;
+        actionText.text = "<color=yellow>"+ "(E)" +"</color>"+"  GET  " +hitInfo.transform.GetComponent<PickUP>().item.itemName; //아이템 정보 텍스트
     }
 
     private void HideInfo()
     {
         pickupActivated = false;
-        actionText.gameObject.SetActive(false);
+        actionText.gameObject.SetActive(false); //아이템을 불러오질 않을때는 텍스트 비활성화
     }
 
     private void PickUp()
     {
-        if (pickupActivated)
+        if (pickupActivated) //아이템이 중앙에 인식했을때
         {
             if (hitInfo.transform != null)
             {
-                if (hitInfo.transform.GetComponent<PickUP>().item.itemType==Item.ItemType.ammo)
+                if (hitInfo.transform.GetComponent<PickUP>().item.itemType==Item.ItemType.ammo) //만약 아이템 타입이 탄약이라면
                 {
-                    theInventory.GetItem(hitInfo.transform.GetComponent<PickUP>().item, 20);
-                    Destroy(hitInfo.transform.gameObject);
+                    theInventory.GetItem(hitInfo.transform.GetComponent<PickUP>().item, 20); //20개를 주움
+                    Destroy(hitInfo.transform.gameObject); //줍고나면 필드에 있는 아이템은 삭제
                     HideInfo();
                 }
                 else
                 {
-                    theInventory.GetItem(hitInfo.transform.GetComponent<PickUP>().item, 1);
+                    theInventory.GetItem(hitInfo.transform.GetComponent<PickUP>().item, 1); //그외의 경우는 하나씩 아이템을 집는다
                     Destroy(hitInfo.transform.gameObject);
                     HideInfo();
                 }
