1f00467ef7956c218f9143f8f6165e3f7ebd5697
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-20 19:25:15 +0900

    주석 업데이트

--- FieldofView.cs
+++ FieldofView.cs
@@ -19,7 +19,7 @@ public class FieldofView : MonoBehaviour
 
     void Update()
     {
-        if (!TotalGameManager.isPlayerDead)
+        if (!TotalGameManager.isPlayerDead) //플레이어가 죽지않았을때
         {
             if (thePlayer.isWalk && thePlayer.isCrouch)
             {
@@ -61,12 +61,13 @@ public class FieldofView : MonoBehaviour
                 viewAngle = 720f;
                 viewDistance = 80f;
             }
+            //플레이어의 상태에 따라 시야각 시야거리 매프레임마다 수정
             Sight();  // 매 프레임마다 시야 탐색
         }
         //Debug.Log("isfire"+GunMainController.isfire);
     }
 
-    public Vector3 ReturnPlayerPos()
+    public Vector3 ReturnPlayerPos() //플레이어 위치 반환
     {
         return thePlayer.transform.position;
     }
@@ -74,24 +75,24 @@ public class FieldofView : MonoBehaviour
     public bool Sight()
     {
 
-        Collider[] _target = Physics.OverlapSphere(transform.position, viewDistance, targetMask);
+        Collider[] _target = Physics.OverlapSphere(transform.position, viewDistance, targetMask);//view distance 만큼 구를 그리고 그 안에 있는 콜라이더들을 _target에 저장함
 
         for (int i = 0; i < _target.Length; i++)
         {
-            Transform TargetTransform = _target[i].transform;
-            if (TargetTransform.name == "Player")
+            Transform TargetTransform = _target[i].transform; //타겟의 위치를 저장
+            if (TargetTransform.name == "Player") //타겟의 이름이 플레이어라면
             {
-                Vector3 _direction = (TargetTransform.position - transform.position).normalized;
-                float _angle = Vector3.Angle(_direction, transform.forward);
+                Vector3 _direction = (TargetTransform.position - transform.position).normalized; //타겟위치와 좀비위치 사이의 벡터값을 구함
+                float _angle = Vector3.Angle(_direction, transform.forward); //구한 벡터값과 전방벡터 사이의 각도를 구함
 
-                if (_angle < viewAngle * 0.5f)
+                if (_angle < viewAngle * 0.5f) //그 각도가 기존의 시야각보다 작으면
                 {
                     RaycastHit _hit;
-                    if (Physics.Raycast(transform.position + transform.up, _direction, out _hit, viewDistance))
+                    if (Physics.Raycast(transform.position + transform.up, _direction, out _hit, viewDistance)) //_direction방향으로 빔을 쏨
                     {
                         if (_hit.transform.name == "Player")
                         {
-                            return true;
+                            return true; //true 반환하면 시야각내에 플레이어가 있음
                         }
                     }
                 }
