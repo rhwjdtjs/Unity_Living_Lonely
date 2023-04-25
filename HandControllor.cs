02aa2daf260c92c287afecaa32fff4a7159e0e54
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-25 17:35:17 +0900

    주석 업데이트

--- HandControllor.cs
+++ HandControllor.cs
@@ -10,11 +10,11 @@ public class HandControllor : MeleeWeaponControllor
     [SerializeField] Animator hitanim;
     void Update()
     {
-        if (isActivate)
-            TryAttack();
+        if (isActivate) //해당스크립트가 활성화 중일때
+            TryAttack(); //공격이 가능하게함
             Moving();
     }
-    public void Moving()
+    public void Moving() //무기에 따른 애니메이션
     {
         if (!thePlayer.isRun && thePlayer.isGround)
         {
@@ -49,12 +49,12 @@ public class HandControllor : MeleeWeaponControllor
         else if (!thePlayer.isRun)
             currentMeleeWeapon.anim.SetBool("Run", thePlayer.isRun);
     }
-    public override void MeleeChangeWeapon(MeleeWeapon _closeWeapon)
+    public override void MeleeChangeWeapon(MeleeWeapon _closeWeapon) //무기변경 함수
     {
         base.MeleeChangeWeapon(_closeWeapon);
         isActivate = true;
     }
-    protected override IEnumerator HITCO()
+    protected override IEnumerator HITCO() //타격함수
     {
         while (isSwing)
         {
@@ -65,7 +65,7 @@ public class HandControllor : MeleeWeaponControllor
                 {
                     hitanim.SetTrigger("hit");
                     hitsound.PlayOneShot(currentMeleeWeapon.HitSound);
-                    hitInfo.transform.GetComponent<Creature>().Damage(currentMeleeWeapon.damage, transform.position);
+                    hitInfo.transform.GetComponent<Creature>().Damage(currentMeleeWeapon.damage, transform.position); //크리쳐 스크립트의 데미지 함수를 가져옴
                 }
                 Debug.Log(hitInfo.transform.name);
             }
